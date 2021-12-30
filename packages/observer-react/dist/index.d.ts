a0792c740655860a6c723ebc1505e04fd9d6e4bc
许圣峰 <kricsleo@163.com>
2021-12-30 17:39:53 +0800

    style: .gitignore

--- packages/observer-react/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { IKObserverOptions, IObserveChildValue } from '@kricsleo/observer';
-export declare function useObserverRoot(key: string, options?: IKObserverOptions): void;
-export declare function useObserver(key: string, el: Element, options: IObserveChildValue): void;
